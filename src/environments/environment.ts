a04d9c18153876aca94d545ff09ffa7f63dcb475
DarwingSanchez <darwing.sanchez@imagineapps.co>
2023-07-20 09:36:56 -0500

    link backend environments configured

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  HOST: 'https://backend-kioskofy.onrender.com',
+  HOST: 'http://localhost:8080',
   gmaps_key: 'AIzaSyAIDSFRWmo4yNVf9hE4Yhk4C93SeBTsqwo',
 };
